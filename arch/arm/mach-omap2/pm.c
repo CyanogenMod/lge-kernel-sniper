2361e5f291251344954ac0a4b33b323f8aab5886
Steve Schefter <sschefter@ti.com>
2011-05-24 22:33:13 -0400

    OMAP3: PM: add /sys/power entries
    This patch adds support for /sys/power entries dsp_freq, vdd1_opp,
    vdd2_opp, vdd1_lock and vdd2_lock for control and reporting of the DSP
    frequency and VDD1/VDD2 OPPs.
    
    Change-Id: I80d56f614a0a72ab03af0d2e4c43100ba3056cab
    Signed-off-by: Steve Schefter <sschefter@ti.com>

--- arch/arm/mach-omap2/pm.c
+++ arch/arm/mach-omap2/pm.c
@@ -187,6 +187,193 @@ struct device *omap4_get_dsp_device(void)
 }
 EXPORT_SYMBOL(omap4_get_dsp_device);
 
+#ifdef CONFIG_OMAP_PM
+static ssize_t vdd_opp_show(struct kobject *, struct kobj_attribute *, char *);
+static ssize_t vdd_opp_store(struct kobject *k, struct kobj_attribute *,
+			  const char *buf, size_t n);
+
+static struct kobj_attribute vdd1_opp_attr =
+	__ATTR(vdd1_opp, 0444, vdd_opp_show, vdd_opp_store);
+static struct kobj_attribute vdd2_opp_attr =
+	__ATTR(vdd2_opp, 0444, vdd_opp_show, vdd_opp_store);
+static struct kobj_attribute vdd1_lock_attr =
+	__ATTR(vdd1_lock, 0644, vdd_opp_show, vdd_opp_store);
+static struct kobj_attribute vdd2_lock_attr =
+	__ATTR(vdd2_lock, 0644, vdd_opp_show, vdd_opp_store);
+static struct kobj_attribute dsp_freq_attr =
+	__ATTR(dsp_freq, 0644, vdd_opp_show, vdd_opp_store);
+
+static int vdd1_locked = 0;
+static int vdd2_locked = 0;
+static struct device sysfs_cpufreq_dev;
+
+static ssize_t vdd_opp_show(struct kobject *kobj, struct kobj_attribute *attr,
+			 char *buf)
+{
+	if (attr == &vdd1_opp_attr)
+		return sprintf(buf, "%hu\n", opp_find_freq_exact(mpu_dev, opp_get_rate(mpu_dev), true)->opp_id+1);
+	else if (attr == &vdd2_opp_attr)
+		return sprintf(buf, "%hu\n", opp_find_freq_exact(l3_dev, opp_get_rate(l3_dev), true)->opp_id+1);
+	else if (attr == &vdd1_lock_attr)
+		return sprintf(buf, "%hu\n", vdd1_locked);
+	else if (attr == &vdd2_lock_attr)
+		return sprintf(buf, "%hu\n", vdd2_locked);
+	else if (attr == &dsp_freq_attr)
+		return sprintf(buf, "%lu\n", opp_get_rate(iva_dev)/1000);
+	else
+		return -EINVAL;
+}
+
+static ssize_t vdd_opp_store(struct kobject *kobj, struct kobj_attribute *attr,
+			  const char *buf, size_t n)
+{
+	unsigned long value;
+
+	if (sscanf(buf, "%lu", &value) != 1)
+		return -EINVAL;
+
+	/* Check locks */
+	if (attr == &vdd1_lock_attr) {
+		if (vdd1_locked) {
+			/* vdd1 currently locked */
+			if (value == 0) {
+				if (omap_pm_set_min_mpu_freq(&sysfs_cpufreq_dev, -1)) {
+					printk(KERN_ERR "%s: Failed to remove vdd1_lock\n", __func__);
+				} else {
+					vdd1_locked = 0;
+					return n;
+				}
+			} else {
+				printk(KERN_ERR "%s: vdd1 already locked to %d\n", __func__, vdd1_locked);
+				return -EINVAL;
+			}
+		} else {
+			/* vdd1 currently unlocked */
+			if (value != 0) {
+				u8 i = 0;
+				unsigned long freq = 0;
+				struct cpufreq_frequency_table *freq_table = *omap_pm_cpu_get_freq_table();
+				if (freq_table == NULL) {
+					printk(KERN_ERR "%s: Could not get freq_table\n", __func__);
+					return -ENODEV;
+				}
+				for (i = 0; freq_table[i].frequency != CPUFREQ_TABLE_END; i++) {
+					if (freq_table[i].index == value - 1) {
+						freq = freq_table[i].frequency;
+						break;
+					}
+				}
+				if (freq_table[i].frequency == CPUFREQ_TABLE_END) {
+					printk(KERN_ERR "%s: Invalid value [0..%d]\n", __func__, i-1);
+					return -EINVAL;
+				}
+				if (omap_pm_set_min_mpu_freq(&sysfs_cpufreq_dev, freq * 1000)) {
+					printk(KERN_ERR "%s: Failed to add vdd1_lock\n", __func__);
+				} else {
+					vdd1_locked = value;
+				}
+			} else {
+				printk(KERN_ERR "%s: vdd1 already unlocked\n", __func__);
+				return -EINVAL;
+			}
+		}
+	} else if (attr == &vdd2_lock_attr) {
+		if (vdd2_locked) {
+			/* vdd2 currently locked */
+			if (value == 0) {
+				if (omap_pm_set_min_bus_tput(&sysfs_cpufreq_dev, OCP_INITIATOR_AGENT, 0)) {
+					printk(KERN_ERR "%s: Failed to remove vdd2_lock\n", __func__);
+				} else {
+					vdd2_locked = 0;
+					return n;
+				}
+			} else {
+				printk(KERN_ERR "%s: vdd2 already locked to %d\n", __func__, vdd2_locked);
+				return -EINVAL;
+			}
+		} else {
+			/* vdd2 currently unlocked */
+			if (value != 0) {
+				unsigned long freq = 0;
+				if (cpu_is_omap3630()) {
+					if(value == 1) {
+						freq = 100*1000*4;
+					} else if (value == 2) {
+						freq = 200*1000*4;
+					} else {
+						printk(KERN_ERR "%s: Invalid value [1,2]\n", __func__);
+						return -EINVAL;
+					}
+				}
+				else if (cpu_is_omap44xx()) {
+					if (omap_rev() <= OMAP4430_REV_ES2_0) {
+						if(value == 1) {
+							freq = 100*1000*4;
+						} else if (value == 2) {
+							freq = 200*1000*4;
+						} else {
+							printk(KERN_ERR "%s: Invalid value [1,2]\n", __func__);
+							return -EINVAL;
+						}
+					} else {
+						if(value == 1) {
+							freq = 98304*4;
+						} else if (value == 2) {
+							freq = 100*1000*4;
+						} else if (value == 3) {
+							freq = 200*1000*4;
+						} else {
+							printk(KERN_ERR "%s: Invalid value [1,2,3]\n", __func__);
+							return -EINVAL;
+						}
+					}
+				} else {
+					printk(KERN_ERR "%s: Unsupported HW [OMAP3630, OMAP44XX]\n", __func__);
+					return -ENODEV;
+				}
+				if (omap_pm_set_min_bus_tput(&sysfs_cpufreq_dev, OCP_INITIATOR_AGENT, freq)) {
+					printk(KERN_ERR "%s: Failed to add vdd2_lock\n", __func__);
+				} else {
+					vdd2_locked = value;
+				}
+				return n;
+			} else {
+				printk(KERN_ERR "%s: vdd2 already unlocked\n", __func__);
+				return -EINVAL;
+			}
+		}
+	} else if (attr == &dsp_freq_attr) {
+		u8 i, opp_id = 0;
+		struct omap_opp *opp_table = omap_pm_dsp_get_opp_table();
+		if (opp_table == NULL) {
+			printk(KERN_ERR "%s: Could not get dsp opp_table\n", __func__);
+			return -ENODEV;
+		}
+		for (i = 1; opp_table[i].rate; i++) {
+			if (opp_table[i].rate >= value) {
+				opp_id = i;
+				break;
+			}
+		}
+		if (opp_id == 0) {
+			printk(KERN_ERR "%s: Invalid value\n", __func__);
+			return -EINVAL;
+		}
+		omap_pm_dsp_set_min_opp(opp_id);
+
+	} else if (attr == &vdd1_opp_attr) {
+		printk(KERN_ERR "%s: changing vdd1_opp is not supported\n", __func__);
+		return -EINVAL;
+	} else if (attr == &vdd2_opp_attr) {
+		printk(KERN_ERR "%s: changing vdd2_opp is not supported\n", __func__);
+		return -EINVAL;
+	} else {
+		return -EINVAL;
+	}
+	return n;
+}
+#endif
+
 /* static int _init_omap_device(struct omap_hwmod *oh, void *user) */
 static int _init_omap_device(char *name, struct device **new_dev)
 {
@@ -268,6 +455,37 @@ static int __init omap2_common_pm_init(void)
 			return error;
 		}
 	}
+#ifdef CONFIG_OMAP_PM
+	{
+		int error = -EINVAL;
+
+		error = sysfs_create_file(power_kobj, &dsp_freq_attr.attr);
+		if (error) {
+			printk(KERN_ERR "%s: sysfs_create_file(dsp_freq) failed %d\n", __func__, error);
+			return error;
+		}
+		error = sysfs_create_file(power_kobj, &vdd1_opp_attr.attr);
+		if (error) {
+			printk(KERN_ERR "%s: sysfs_create_file(vdd1_opp) failed %d\n", __func__, error);
+			return error;
+		}
+		error = sysfs_create_file(power_kobj, &vdd2_opp_attr.attr);
+		if (error) {
+			printk(KERN_ERR "%s: sysfs_create_file(vdd2_opp) failed %d\n", __func__, error);
+			return error;
+		}
+		error = sysfs_create_file(power_kobj, &vdd1_lock_attr.attr);
+		if (error) {
+			printk(KERN_ERR "%s: sysfs_create_file(vdd1_lock) failed %d\n", __func__ ,error);
+			return error;
+		}
+		error = sysfs_create_file(power_kobj, &vdd2_lock_attr.attr);
+		if (error) {
+			printk(KERN_ERR "%s: sysfs_create_file(vdd2_lock) failed %d\n", __func__, error);
+			return error;
+		}
+	}
+#endif
 
 	return 0;
 }
